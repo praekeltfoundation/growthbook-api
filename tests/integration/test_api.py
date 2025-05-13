7770599efbc00122e221744c32ad79314ca63293
Rudi Giesler <rudi@praekelt.org>
2025-05-13 15:42:47 +0200

    Change to pytest fixtures

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,20 +1,32 @@
 from fastapi.testclient import TestClient
+import pytest
 
 from growthbook_api.main import app, get_gb_client
 from .fakes import FakeGrowthBookClient
 
 
-def test_get_feature_value():
+@pytest.fixture
+def gb_client():
     fake_client = FakeGrowthBookClient()
     app.dependency_overrides[get_gb_client] = lambda: fake_client
+    yield fake_client
+    del app.dependency_overrides[get_gb_client]
+
+
+@pytest.fixture
+def api_client(gb_client):
     with TestClient(app) as client:
-        response = client.post(
-            "/evaluate_feature",
-            json={
-                "feature_key": "prototype-feature",
-                "user_attributes": {"whatsapp_id": "27820001001"},
-            },
-        )
+        yield client
+
+
+def test_get_feature_value(api_client):
+    response = api_client.post(
+        "/evaluate_feature",
+        json={
+            "feature_key": "prototype-feature",
+            "user_attributes": {"whatsapp_id": "27820001001"},
+        },
+    )
     assert response.status_code == 200
     assert response.json() == {
         "experiment": {
@@ -28,13 +40,11 @@ def test_get_feature_value():
     }
 
 
-def test_client_initialized_and_closed():
-    fake_client = FakeGrowthBookClient()
-    app.dependency_overrides[get_gb_client] = lambda: fake_client
-    assert not fake_client.initialized
-    assert not fake_client.closed
+def test_client_initialized_and_closed(gb_client):
+    assert not gb_client.initialized
+    assert not gb_client.closed
     with TestClient(app):
-        assert fake_client.initialized
-        assert not fake_client.closed
-    assert fake_client.initialized
-    assert fake_client.closed
+        assert gb_client.initialized
+        assert not gb_client.closed
+    assert gb_client.initialized
+    assert gb_client.closed
